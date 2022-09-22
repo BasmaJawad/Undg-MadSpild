6874f6e36fc1cf19ceb960d478e71f50fae5f916
BasmaJawad <basm0035@stud.kea.dk>
2022-09-22 11:20:12 +0200

    extra files

--- src/main/java/com/example/undgaamadspild/controller/madspildController.java
+++ src/main/java/com/example/undgaamadspild/controller/madspildController.java
@@ -9,7 +9,22 @@ public class madspildController {
 
     @GetMapping("/")
     public String homepage(){
-        return "";
+        return "homepage";
+    }
+
+    @GetMapping("/info")
+    public String info(){
+        return "info";
+    }
+
+    @GetMapping("/shop")
+    public String shop(){
+        return "shop";
+    }
+
+    @GetMapping("/login")
+    public String login(){
+        return "login";
     }
 
 }
